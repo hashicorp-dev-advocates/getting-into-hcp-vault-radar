b148fbda921f5f1acb030e89a4cf82d00ceba602
Michael Kosir <michael.kosir@hashicorp.com>
2025-08-26 12:28:45 -0400

    more code

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+import base64
+
 import boto3
+import hvac
 
 AWS_ACCESS_KEY_ID = "AKIAE2647DD83A384175"
 AWS_SECRET_ACCESS_KEY = "g4fEPvwN2+2kT8omL4ieFqgVj4xLWx40PxNmOJ3S"
+
+VAULT_ADDR = "http://vault.example.com"
+VAULT_TOKEN = "hvs.4kZkME6mRPIc9HGX3rpVet8p"
+
+vault_client = hvac.Client(url=VAULT_ADDR, token=VAULT_TOKEN)
 
 sts_client = boto3.client(
     service_name="sts",
@@ -16,5 +24,24 @@ def aws():
     print(response)
 
 
+def kv():
+    response = vault_client.secrets.kv.read_secret_version(path="path/to/my/secret")
+    print(response)
+
+
+def encrypt():
+    # encrypt credit card number
+    ccn = base64.b64encode(b"5105-1051-0510-5100").decode()
+    response = vault_client.secrets.transit.encrypt_data(name="example", plaintext=ccn)
+    print(response["data"]["ciphertext"])
+
+    # encrypt social security number
+    ssn = base64.b64encode(b"123-45-6789").decode()
+    response = vault_client.secrets.transit.encrypt_data(name="example", plaintext=ssn)
+    print(response["data"]["ciphertext"])
+
+
 if __name__ == "__main__":
     aws()
+    kv()
+    encrypt()
